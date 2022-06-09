db6c89560e21e7201b0dac3a8d8bab7b0b5975bf
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-06-10 00:40:37 +0530

    Time: 8 ms (51.41%), Space: 7.7 MB (21.96%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -5,12 +5,11 @@ public:
         string temp = "";
         for(int i = 0 ; i < s.length() ; i++)
         {
-            while(isalnum(s[i]))
+            if(isalnum(s[i]))
             {
                 if(isupper(s[i]))
                     s[i]= tolower(s[i]);
                 temp += s[i];
-                i++;
             }
         }
         int n = temp.length();
