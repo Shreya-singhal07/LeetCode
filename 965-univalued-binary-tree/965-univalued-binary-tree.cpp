2c0f995abec93195d63d8993c14f485ada99102f
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-06-13 16:01:39 +0530

    Time: 8 ms (17.22%), Space: 9.9 MB (44.92%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.cpp
+++ 965-univalued-binary-tree/965-univalued-binary-tree.cpp
@@ -9,18 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     bool isUnivalTree(TreeNode* root) 
     {
-        if(root == nullptr) 
-            return true;
+        while((root->left==NULL || root->left->val==root->val && isUnivalTree(root->left))            && (root->right==NULL || root->right->val==root->val && isUnivalTree(root->right)))
+          return true;
         
-        if(root->left != nullptr && root->left->val != root->val)
-            return false;
-        if(root->right != nullptr && root->right->val != root->val) 
-            return false;
-        
-        return isUnivalTree(root->left) && isUnivalTree(root->right);
+        return false;    
     }
 };
+
+// class Solution {
+// public:
+//     bool isUnivalTree(TreeNode* root) 
+//     {
+//         if(root == nullptr) 
+//             return true;
+        
+//         if(root->left != nullptr && root->left->val != root->val)
+//             return false;
+//         if(root->right != nullptr && root->right->val != root->val) 
+//             return false;
+        
+//         return isUnivalTree(root->left) && isUnivalTree(root->right);
+//     }
+// };
