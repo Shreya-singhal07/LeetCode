e032164f1572d578c6f6440e1433350bf5e126a4
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-06-15 14:23:25 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (71.69%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        for(int i=m ; i<m+n ; i++)
+        {
+            nums1.pop_back();
+        }
+        for(int i=0 ; i<n; i++)
+        {
+                nums1.push_back(nums2[i]);
+        }
+        
+        sort(nums1.begin(),nums1.end());
+    }
+};
+
+
+
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+//     {
+//         int i=0,j=0;
+     
+//         vector<int>temp;
+//         while(i<m &&j<n)
+//         {
+//             if(nums1[i]<=nums2[j])
+//             {
+//                 temp.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else
+//             {
+//                 temp.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+//         while(i<m)
+//         {
+//             temp.push_back(nums1[i]);
+//             i++; 
+//         }
+//         while(j<n)
+//         {
+//             temp.push_back(nums2[j]);
+//             j++;
+//         }
+//         nums1=temp;
+//     }
+// };
