7385c998e85abf48876aa9206caa112911f080bd
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-06-22 13:11:13 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.64%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,44 @@
+//OPTIMISED SOLUTION [DP]
+
 class Solution {
 public:
+    int solve(vector<int>&dp, int n) //pass by reference
+    {
+        if(dp[n] != -1)
+            return dp[n];
+        else
+        {
+            int res;
+            if(n == 0 || n == 1)
+                res = n;
+            else
+                res = solve(dp,n-1) + solve(dp,n-2);
+            dp[n] = res;
+        }
+        return  dp[n];
+            
+    }
+    
     int fib(int n) 
     {
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
-       return  fib(n-1)+ fib(n-2);
-        
+        vector<int>dp (n+1,-1); 
+        return solve(dp,n); 
     }
 };
+
+
+
+//RECURSIVE SOLUTION
+
+// class Solution {
+// public:
+//     int fib(int n) 
+//     {
+//         if(n == 0)
+//             return 0;
+//         if(n == 1)
+//             return 1;
+//        return  fib(n-1)+ fib(n-2);
+        
+//     }
+// };
