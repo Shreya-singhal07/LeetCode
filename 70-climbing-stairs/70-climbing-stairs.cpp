1d54516145b58e6cf7da5c3f7a5e7dd8e0052275
shreya singhal <69424250+Shreya-singhal07@users.noreply.github.com>
2022-06-22 14:10:43 +0530

    Time: 5 ms (10.90%), Space: 6.1 MB (30.44%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,39 @@
+class Solution 
+{
+public:
+   
+    int solve(vector<int>&dp, int n)
+    {
+        if(dp[n] == -1)
+        {
+            if(n<=2)
+                return n;
+            if(dp[n] != -1)
+                return dp[n];
+            int ways1 = solve(dp,n-1);
+            int ways2 = solve(dp,n-2);
+            dp[n] = ways1 + ways2;   
+        }
+        
+        return dp[n];
+    }
+    
+    int climbStairs(int n) 
+    {
+        vector<int>dp(n+1,-1);
+        return solve(dp,n);
+    }
+};
+
+// class Solution {
+// public:
+    
+//     int climbStairs(int n) 
+//     {
+//          if(n<=3)
+//             return n;
+        
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
+
